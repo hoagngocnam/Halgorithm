2890446f78706bdf6f299b7356f6ed9b9a515611
HoangNgocNam <nam.hoangngoc@vti.com.vn>
2023-02-17 18:15:10 +0700

    bb

--- www/Modules/Users/Resources/views/authenticate/login.blade.php
+++ www/Modules/Users/Resources/views/authenticate/login.blade.php
@@ -1,5 +1,7 @@
-@extends('users::layouts.index')
-
-@section('main-content')
-  
-@endsection
+<form action="" method="POST">
+    <label for="fname">Tài khoản:</label><br>
+    <input type="text" id="fname" name="mail_adr" value=""><br>
+    <label for="lname">Mật khẩu:</label><br>
+    <input type="text" id="lname" name="password" value=""><br><br>
+    <input type="submit" value="Submit">
+</form>
--- www/Modules/Users/Routes/web.php
+++ www/Modules/Users/Routes/web.php
@@ -14,15 +14,31 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::name('user.')->group(function () {
+    Route::withoutMiddleware(['authenticate:admin'])->name('authenticate.')->group(function () {
+        Route::match(['get', 'post'], 'login', 'AuthenticateController@login')->name('login');
+        Route::match(['get', 'post'], 'register', 'AuthenticateController@register')->name('register');
+        Route::get('logout', 'AuthenticateController@logout')->name('logout');
+    });
+
     Route::get('', 'HomeController@index')->name('users.home');
+
     Route::get('shop/{shop_id}', 'ShopController@index')->name('users.shop');
+
     Route::get('{category_id}', 'CategoryController@index')->name('users.category')->whereNumber(['category_id']);
-    Route::get('{category_id}/{product_id}', 'ProductController@index')->name('users.product')->whereNumber('category_id')->whereNumber('product_id');
+
+    Route::get('{category_id}/{product_id}',
+        'ProductController@index')->name('users.product')->whereNumber('category_id')->whereNumber('product_id');
+
     Route::get('cart', 'CartController@index')->name('users.cart');
+
     Route::get('create-payment', 'CartController@create')->name('users.create-payment');
+
     Route::get('return-vnpay', 'CartController@returnVnpay')->name('users.return-vnpay');
+
 // Route::get('cart/success', 'CartController@success')->name('users.cart.success');
+
     Route::get('cart/paygate/1', 'CartController@paygate')->name('users.cart.paygate');
+
     Route::name('users.')->group(function () {
         Route::get('{category_id}/{product_id}', 'ProductController@index')->name('product');
     });
